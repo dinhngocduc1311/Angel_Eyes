2f669eac726b02919ebf8790789fc43ec01313aa
chikacow <125446522+chikacow@users.noreply.github.com>
2024-12-09 16:44:05 +0700

    update text_to_speech module

--- Angel_Eyes/lib/text_to_speech.dart
+++ Angel_Eyes/lib/text_to_speech.dart
@@ -3,10 +3,13 @@ import 'package:flutter_tts/flutter_tts.dart';
 FlutterTts flutterTts = FlutterTts();
 
 Future<void> speak(String textToSpeak) async {
-  await flutterTts.setLanguage("en-US");
+  await flutterTts.setLanguage("en-AU");
   await flutterTts.setSpeechRate(0.5);
   await flutterTts.setVolume(1.0);
-  await flutterTts.setPitch(1);
+  await flutterTts.setPitch(0.7);
+
+ //await flutterTts.setVoice({"name": "male_voice_name", "locale": "en-AU"});
+  
   try {
     var res = await flutterTts.speak(textToSpeak);
     if (res == 1) {
